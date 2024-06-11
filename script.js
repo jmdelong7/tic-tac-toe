33a3c7ee007eff798257402f67f4888e2135016e
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-06-10 20:34:37 -0700

    Gameboard dimension creation.

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ function Gameboard() {
   const gameboard = [];
   const dimension = 3;
 
-  for (let i = 0; i < (dimension * dimension); i++) {
+  for (let i = 0; i < dimension; i++) {
     gameboard.push([])
+    
+    for (let j = 0; j < dimension; j++) {
+      gameboard[i].push([]);
+    }
+
   }
+
+
+
+  // get 0 - dimension = index x index
+
 
   const getGameboard = () => gameboard;
 
