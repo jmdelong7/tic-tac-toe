da73c28c8c772d949281b0c3c7b8346848610831
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-06-12 21:10:11 -0700

    grid, grid.addSymbol() and playerSelection()
    Different from starting last time, grid has method to add symbol.
    Method is called in Controller() under playerSelection().
    Adds symbol to grid without exposing grid publicly.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # tic-tac-toe
+
+Use the revealing module pattern and factory functions to create the game.
+- grid
+- 2 players
+- 2 symbols
+- turns
+- player position selection on the grid
+- A position can be selected once per game by 1 player
+- 3 in a row wins
+
+GRID / CONTROLLER / PLAYERS
+
+GRID
+- Private
+  - Main grid held in an array.
+  - Add player symbol based on position choice to array.
+- Public
+  - Return grid array.
+
+CONTROLLER
+- Private
+  - Player turn. 
+  - Win case.
+- Public
+  - Player selection.
+
+PLAYER
+- Private
+  - Player name & symbol.
---
+++ script.js
@@ -0,0 +1,34 @@
+const grid = (function Grid() {
+
+  const gridArr = [[],[],[]];
+
+  // Symbol needs to be correct player's symbol
+  // gridArr needs to be player's choice
+  function addSymbol(row, col, symbol){
+    return gridArr[row][col] = symbol;
+  }
+
+  function displayGrid() {
+    return gridArr;
+  }
+
+  return { addSymbol, displayGrid }
+
+})()
+
+function Controller() {
+
+  let turn;
+  let position;
+
+  function playerSelection(player, position) {
+    if (position >= 0 && position < 3) {
+      grid.addSymbol(0, position, player.symbol)
+    } else if (position >= 3 && position < 5){
+      grid.addSymbol(1, position - 3, player.symbol)
+    } else if (position >= 5 && position < 8) {
+      grid.addSymbol(2, position - 6, player.symbol)
+    }
+  }
+
+}
